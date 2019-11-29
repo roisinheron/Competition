53f0a5dcf7e0c2bf270ef1fa4249bdd717425fa5
natbibi <58271566+natbibi@users.noreply.github.com>
2019-11-29 15:48:07 +0000

    youtube video

--- olafur-page.js
+++ olafur-page.js
@@ -1,4 +1,20 @@
-$( "h2" ).hover(function() {
-  $( this ).fadeOut( 1000 );
-  $( this ).fadeIn( 1000 );
+$("h2").hover(function () {
+  $(this).fadeOut(1000);
+  $(this).fadeIn(1000);
 });
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
